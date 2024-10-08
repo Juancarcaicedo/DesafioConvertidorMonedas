e1bd58a1fd9be80082b058b33c15788f25268038
Juancarcaicedo <74690988+Juancarcaicedo@users.noreply.github.com>
2024-10-08 16:36:35 -0500

    Menu Actualizado

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,5 @@
 import Menu.Menu;
 
-import java.util.Scanner;
-
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
