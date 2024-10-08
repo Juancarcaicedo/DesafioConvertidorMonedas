a738fc733e87809b1bec1e9d8d2e1a435849d128
Juancarcaicedo <74690988+Juancarcaicedo@users.noreply.github.com>
2024-10-08 15:33:18 -0500

    IMPLEMENTACION V1 MENU

---
+++ src/Menu/Menu.java
@@ -0,0 +1,48 @@
+package Menu;
+import java.util.Scanner;
+
+public class Menu {
+
+    public void mostrarMenu() {
+        Scanner scanner = new Scanner(System.in);
+        int option;
+        do {
+            System.out.println("\n*** Menú de opciones ***");
+            System.out.println("1) Dolar =>> Peso Argentino");
+            System.out.println("2) Peso Argentino =>> Dolar");
+            System.out.println("3) Dolar =>> Real Brasileño");
+            System.out.println("4) Real Brasileño =>> Dolar");
+            System.out.println("5) Dolar =>> Peso Colombiano");
+            System.out.println("6) Peso Colombiano =>> Dolar");
+            System.out.println("7. Salir");
+            System.out.print("Elige una opción: ");
+            option = scanner.nextInt();
+            switch (option) {
+                case 1:
+                    System.out.println("Dolar =>> Peso Argentino");
+                    break;
+                case 2:
+                    System.out.println("Peso Argentino =>> Dolar");
+                    break;
+                case 3:
+                    System.out.println("Dolar =>> Real Brasileño");
+                    break;
+                case 4:
+                    System.out.println("Real Brasileño =>> Dolar");
+                    break;
+                case 5:
+                    System.out.println("Dolar =>> Peso Colombiano");
+                    break;
+                case 6:
+                    System.out.println("Peso Colombiano =>> Dolar");
+                    break;
+                case 7:
+                    System.out.println("Saliendo del menú...");
+                    break;
+                default:
+                    System.out.println("Opción inválida. Por favor, elige una opción válida.");
+            }
+        } while (option != 7);
+        scanner.close();
+    }
+}
